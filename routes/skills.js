d69b0bcfe40301c94056a49a1821ce5a65c0cc1b
williuhm-codes <whu54@uwo.ca>
2023-08-01 18:02:13 -0400

    controller working

--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var skillsCtrl = require("../controllers/skills")
 
 // All actual path s start with "/skills"
+
+// GET /todos
+router.get("/", skillsCtrl.index)
 
 module.exports = router;
