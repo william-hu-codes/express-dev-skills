cfa8d8e18538ac2c5a15dbcd4e26efe083dd8049
williuhm-codes <whu54@uwo.ca>
2023-08-01 16:12:03 -0400

    add skill model module

---
+++ models/skill.js
@@ -0,0 +1,22 @@
+const skills = {
+    JavaScript: {
+        id: 1,
+        learned: true
+    },
+    HTML: {
+        id: 2,
+        learned: true
+    },
+    CSS: {
+        id: 3,
+        learned: true
+    },
+    Python: {
+        id: 4,
+        learned: false
+    },
+    Mongoose: {
+        id: 5,
+        learned: false
+    }
+}
