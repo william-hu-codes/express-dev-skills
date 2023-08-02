eecdc05792dd685005508e02bd0f600ce78675ec
williuhm-codes <whu54@uwo.ca>
2023-08-02 15:33:05 -0400

    add time to res.locals using middleware and innject into header

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//custom middleware
+app.use(function(req, res, next) {
+  res.locals.time = new Date().toLocaleTimeString()
+  next()
+})
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
