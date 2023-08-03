3e289f70043b05800040fb34cc77f14a72beb191
williuhm-codes <whu54@uwo.ca>
2023-08-03 15:59:26 -0400

    change styling of show  ejs

--- controllers/skills.js
+++ controllers/skills.js
@@ -19,7 +19,8 @@ function index(req, res) {
 
 function show(req, res) {
     res.render("skills/show", {
-        skill: Skill.getOne(req.params.id)
+        skill: Skill.getOne(req.params.id),
+        title: "Skill:"
     })
 }
 
